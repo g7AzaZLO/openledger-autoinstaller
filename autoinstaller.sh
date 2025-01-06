e8dd600bcd54e0c09118559c784658a726f30ec3
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2025-01-06 12:52:33 +0300

    Update autoinstaller.sh

--- autoinstaller.sh
+++ autoinstaller.sh
@@ -4,7 +4,9 @@
 . <(wget -qO- https://raw.githubusercontent.com/g7AzaZLO/server_primary_setting/refs/heads/main/server_primary_setting)
 
 # Установка необходимых пакетов
-sudo apt install -y libasound2 libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libsecret-1-0 desktop-file-utils libgbm1
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu jammy main universe"
+sudo apt-get install libasound2=1.2.6.1-1ubuntu1 libasound2-data=1.2.6.1-1ubuntu1
+sudo apt install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libsecret-1-0 desktop-file-utils libgbm1
 
 # Скачивание и распаковка OpenLedger Node
 wget https://cdn.openledger.xyz/openledger-node-1.0.0-linux.zip
