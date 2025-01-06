44f1dbe386871e3f6881229aabc0c4a9f8d9c3ed
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2025-01-06 13:17:30 +0300

    Update autoinstaller.sh

--- autoinstaller.sh
+++ autoinstaller.sh
@@ -4,13 +4,14 @@
 . <(wget -qO- https://raw.githubusercontent.com/g7AzaZLO/server_primary_setting/refs/heads/main/server_primary_setting)
 
 # Установка необходимых пакетов
-sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu jammy main universe" -y
-sudo apt-get update
-sudo apt-get install -y libatspi2.0-0
 
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu jammy main universe"
 sudo apt-get install -y libasound2=1.2.6.1-1ubuntu1 libasound2-data=1.2.6.1-1ubuntu1
-sudo apt install -y libatspi2.0-0 libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libsecret-1-0 desktop-file-utils libgbm1
+sudo apt install -y libgtk-3-0 libnotify4 libnss3 libxss1 libxtst6 xdg-utils libatspi2.0-0 libsecret-1-0 desktop-file-utils libgbm1
+
+wget http://archive.ubuntu.com/ubuntu/pool/main/a/at-spi2-core/libatspi2.0-0_2.44.1-0ubuntu2_amd64.deb
+sudo dpkg -i libatspi2.0-0_2.44.1-0ubuntu2_amd64.deb
+sudo apt-get install -f
 
 # Скачивание и распаковка OpenLedger Node
 wget https://cdn.openledger.xyz/openledger-node-1.0.0-linux.zip
